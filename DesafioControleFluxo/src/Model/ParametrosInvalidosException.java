399efa73c7ca2af9caa6f1b0566618f8783e33e2
Andrew <andrewfreiria28@gmail.com>
2025-07-01 12:25:08 -0300

    feat: cria classe Contador e exception ParametrosInvalidosException

---
+++ DesafioControleFluxo/src/Model/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class ParametrosInvalidosException extends RuntimeException {
+    public ParametrosInvalidosException(final String message) {
+        super(message);
+    }
+}
