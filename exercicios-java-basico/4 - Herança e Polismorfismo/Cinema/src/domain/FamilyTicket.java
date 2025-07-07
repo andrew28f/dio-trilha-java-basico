76d5fdc367834bc6574a884f2006c3219f568462
Andrew <andrewfreiria28@gmail.com>
2025-07-07 18:43:16 -0300

    refactor(FamilyTicket): altera o modificador de acessibilidade das propriedades para private

--- exercicios-java-basico/4 - Herança e Polismorfismo/Cinema/src/domain/FamilyTicket.java
+++ exercicios-java-basico/4 - Herança e Polismorfismo/Cinema/src/domain/FamilyTicket.java
@@ -2,8 +2,8 @@ package domain;
 
 public non-sealed class FamilyTicket extends Ticket {
 
-    int numberOfPeople;
-    int discount;
+    private int numberOfPeople;
+    private final double discount;
 
     public FamilyTicket(double price, String name, boolean isSubtitled, int numberOfPeople) {
         super(price, name, isSubtitled);
