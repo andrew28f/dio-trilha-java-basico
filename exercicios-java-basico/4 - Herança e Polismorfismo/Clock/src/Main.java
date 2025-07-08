5323663dd44bdc27b52c499785a876532a010426
Andrew <andrewfreiria28@gmail.com>
2025-07-08 09:13:04 -0300

    feat: adiciona e implementa classe Main para demonstrar funcionamento do codigo

---
+++ exercicios-java-basico/4 - Herança e Polismorfismo/Clock/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Clock brlClock = new BRLClock();
+        brlClock.setSecond(80);
+        brlClock.setMinute(25);
+        brlClock.setHour(13);
+
+        System.out.println(brlClock.getTime());
+
+        System.out.println(new USClock().convertTime(brlClock).getTime());;
+
+    }
+}
